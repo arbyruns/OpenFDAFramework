fdb32bd6c6f0d6bb220a62c6097921b448603f24
Rob Evans <robevans@microsoft.com>
2023-08-17 09:34:54 -0400

    Initial Commit

---
+++ Tests/OpenFDAFrameworkTests/OpenFDAFrameworkTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import OpenFDAFramework
+
+final class OpenFDAFrameworkTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(OpenFDAFramework().text, "Hello, World!")
+    }
+}
